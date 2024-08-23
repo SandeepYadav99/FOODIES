810ebea5671c19a7e456fcc478cf6c0285515e0c
SandeepYadav99 <sandeepyadav.7one@gmail.com>
2024-08-23 16:03:01 +0530

    [feature] sql light db

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,9 +2,17 @@ import Link from 'next/link';
 
 import classes from './page.module.css';
 import MealsGrid from '../components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
+import { Suspense } from 'react';
 
+async function Meals() {
+  const meals = await getMeals();
 
-const  MealsPage=()=> {
+  return <MealsGrid meals={meals} />;
+}
+
+const  MealsPage=async()=> {
+  const meals = await getMeals();
   return (
     <>
       <header className={classes.header}>
@@ -21,8 +29,13 @@ const  MealsPage=()=> {
           </Link>
         </p>
       </header>
-      <main className={classes.main}>
-        <MealsGrid meals={[]} />
+      {/* <main className={classes.main}>
+        <MealsGrid meals={meals} />
+      </main> */}
+       <main className={classes.main}>
+        <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
